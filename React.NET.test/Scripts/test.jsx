ee0319f28ed07cf52ecaaec36693b3917cf6b381
bjvdnbus <bjorn.vandenbussche@ugent.be>
2015-05-02 14:17:01 +0200

    comments added

--- React.NET.test/Scripts/test.jsx
+++ React.NET.test/Scripts/test.jsx
@@ -2,11 +2,49 @@
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 React.render(
   <CommentBox />,
   document.getElementById('content')
