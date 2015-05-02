07a22f030e1a809683babdef17b0b8485ba51f08
bjvdnbus <bjorn.vandenbussche@ugent.be>
2015-05-02 14:19:43 +0200

    changed to JSON data

--- React.NET.test/Scripts/test.jsx
+++ React.NET.test/Scripts/test.jsx
@@ -1,9 +1,15 @@
-﻿var CommentBox = React.createClass({
+﻿var data = [
+  { Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+  { Author: "Pete Hunt", Text: "This is one comment" },
+  { Author: "Jordan Walke", Text: "This is *another* comment" }
+];
+
+var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -27,11 +33,16 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.Author}>
+          {comment.Text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -48,6 +59,6 @@ var CommentForm = React.createClass({
 });
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
